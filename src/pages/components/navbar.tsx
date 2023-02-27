ab601b80e8ef443fd8c08a67418092d58b183600
Raees Aziz Muhammad <37083651+raees21@users.noreply.github.com>
2023-02-27 12:10:45 +0200

    map tasks to tasklist

--- src/pages/components/navbar.tsx
+++ src/pages/components/navbar.tsx
@@ -3,7 +3,7 @@ import navStyle from "./navbar.module.css"
 
 const Navbar = () => {
     return (
-        <div className="todo-app" >
+        <div className={navStyle.todoApp} >
         <div className="container">
             <div className={navStyle.image}>
                 <img  src="./check-list-doodle.jpg" width="125" height="125" />
@@ -11,7 +11,7 @@ const Navbar = () => {
             <h1 className={navStyle.toDo}>To do List</h1>
         </div>
         <div className={navStyle.navbar}>
-            <Link href="/">
+            <Link href="/" className={navStyle.link}>
                 Home
             </Link>
         </div>
--- src/pages/components/tasklist.tsx
+++ src/pages/components/tasklist.tsx
@@ -1,6 +1,18 @@
+import { useState } from "react";
 import tscss from "./tasklist.module.css"
 
+const taskArray:string[] = [];
+
 const Tasklist = () => {
+    const [tasks, settasks] = useState(taskArray)
+
+    const addTask = () => {
+      settasks([
+        ...tasks, 
+        "hello"
+      ]);
+    };
+
     return (
         <table id={tscss.tasklist}>
         <thead>
@@ -11,16 +23,19 @@ const Tasklist = () => {
         <tbody>
         <tr>
           <td>
-            <button id={tscss.done} className="fa fa-check" ></button>
-            <button id={tscss.delete} className="fa fa-trash"></button>
+            {/* <button id={tscss.done} className="fa fa-check" ></button> */}
+            {tasks.map((t) => (
+            <p>{t}</p>
+          ))}
+            {/* <button id={tscss.delete} className="fa fa-trash"></button>
             <button id={tscss.archive} className="fa fa-archive"></button>
-            <button id={tscss.edit} className="fa fa-pencil"></button>
+            <button id={tscss.edit} className="fa fa-pencil"></button> */}
     
             <div>
               {/* <!-- add a model for your input so on the text box it shoes something.. google model for input  --> */}
               <input type="text" contentEditable="true" /> 
               
-              <button className={tscss.donebutton}>Done</button>
+              <button onClick={addTask} className={tscss.donebutton}>Done</button>
             </div>
           </td>
         </tr>
